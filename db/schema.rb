97748ea80ea720d4757c56e269478fd07276d5d1
Adi Zimmerman <adizim@berkeley.edu>
2017-04-09 15:17:15 -0700

    woops

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170401165355) do
-=======
 ActiveRecord::Schema.define(version: 20170409210724) do
->>>>>>> e58172a5d2266add04b27363a2b1111fb5f8b86e
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -49,8 +45,6 @@ ActiveRecord::Schema.define(version: 20170409210724) do
     t.string  "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
-<<<<<<< HEAD
-=======
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,7 +62,6 @@ ActiveRecord::Schema.define(version: 20170409210724) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> e58172a5d2266add04b27363a2b1111fb5f8b86e
   end
 
 end
