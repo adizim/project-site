265c5ee6a6c3fca9b95ab05b081f6a64827c43d6
Yitian Zou <yitian.zou@berkeley.edu>
2017-04-11 18:57:30 -0700

    make new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,23 @@ class ProjectsController < ApplicationController
 			@projects = Project.all
 		end
 	end
+
+	def show
+		@project = Project.find(params[:id])
+	end 
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(project_params)
+		@project.save
+		redirect_to new_project_path
+	end
+
+	private
+		def project_params
+			params.require(:project).permit(:title, :summary)
+		end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get 'contact', to: "pages#contact"
   get 'apply', to: "pages#apply"
   get 'account', to: "pages#account"
-  #resources :projects
-  get 'projects', to: "projects#index"
+  resources :projects
+  #get 'projects', to: "projects#index"
   get 'tags/:tag', to:'projects#index', as: :tag 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
