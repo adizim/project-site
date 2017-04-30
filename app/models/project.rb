22cc82941e763fdb6e04e137653ab0b28bce9666
Adi Zimmerman <adizim@berkeley.edu>
2017-04-30 15:57:22 -0700

    deleted projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,8 @@ class Project < ApplicationRecord
 	serialize :tags, Array
 	acts_as_taggable
 	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200", robot: "640x320" }
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates_attachment_content_type :image, :content_type => /^image\/(png|gif|jpeg|jpg)/, :message => 'only (png/gif/jpeg/jpg) images'
+
 
 
 end
