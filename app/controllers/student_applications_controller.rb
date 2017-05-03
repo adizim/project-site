1dcf278bea0a24730310fac6717a034d3d21b9d9
Yitian Zou <yitian.zou@berkeley.edu>
2017-05-03 10:26:43 -0700

    student app basic form

--- app/controllers/student_applications_controller.rb
+++ app/controllers/student_applications_controller.rb
@@ -4,10 +4,19 @@ class StudentApplicationsController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@student_application = StudentApplication.new
 		@question1 = @project.question1
+		@question2 = @project.question2
+		@question3 = @project.question3
+		@skills = @project.skills.split(" ")
+		@courses = @project.courses.split(" ")
 	end
 
 	def show
 		@app = StudentApplication.find(params[:id])
 		@question1 = @app.project.question1
 	end
+
+	def create
+		
+	end
+
 end
